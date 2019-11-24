e9ee522fb4631dfadeab1843d8b41a962c6d7045
jesusmontano <jesusmontano96@gmail.com>
2019-11-24 14:41:23 -0800

    Updates

---
+++ precache-manifest.82b7114c533a9498f5560aa5bcd2bfb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a0ac45337e7a02f98ef837c58f626b",
+    "url": "/tradeshift-challenge/index.html"
+  },
+  {
+    "revision": "f51ed870c5c3ced66821",
+    "url": "/tradeshift-challenge/static/css/2.17a8fbb8.chunk.css"
+  },
+  {
+    "revision": "978500dd97323d8cc794",
+    "url": "/tradeshift-challenge/static/css/main.b1533579.chunk.css"
+  },
+  {
+    "revision": "f51ed870c5c3ced66821",
+    "url": "/tradeshift-challenge/static/js/2.62457508.chunk.js"
+  },
+  {
+    "revision": "978500dd97323d8cc794",
+    "url": "/tradeshift-challenge/static/js/main.f2d5855f.chunk.js"
+  },
+  {
+    "revision": "18b2354c574c196a4d41",
+    "url": "/tradeshift-challenge/static/js/runtime-main.337f2dbc.js"
+  }
+]);
