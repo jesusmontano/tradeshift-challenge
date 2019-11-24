bcadb2940239705e19a16cbc9f6169a4e4042ee1
jesusmontano <jesusmontano96@gmail.com>
2019-11-24 12:58:05 -0800

    Add triangle type tests

--- src/App.test.js
+++ src/App.test.js
@@ -38,3 +38,36 @@ describe('isTriangle', () => {
   })
 
 })
+
+describe('determineType', () => {
+  it("Returns not possible if the sides violate the triangle inequality theorem.", () => {
+    const triangle = determineType(3, 4, 100);
+    expect(triangle).toEqual('Not Possible');
+
+    const triangle2 = determineType(1, 1, 2);
+    expect(triangle2).toEqual('Not Possible')
+  })
+
+  it("Returns scelene if all sides of the triangle are unequal.", () => {
+    const triangle = determineType(3, 4, 5);
+    expect(triangle).toEqual('Scalene');
+  })
+
+  it("Returns isosceles if exactly 2 sides of the triangle are equal.", () => {
+    const triangle = determineType(3, 3, 5);
+    expect(triangle).toEqual('Isosceles');
+  })
+
+  it("Returns equilateral if all sides of the triangle are equal.", () => {
+    const triangle = determineType(3, 3, 3);
+    expect(triangle).toEqual('Equilateral');
+  })
+
+  it("Accurately handles decimals.", () => {
+    const triangle = determineType(3.0, 3, 3.00000);
+    expect(triangle).toEqual('Equilateral');
+
+    const triangle2 = determineType(2.17, 2.17000, 3);
+    expect(triangle2).toEqual('Isosceles');
+  })
+})
