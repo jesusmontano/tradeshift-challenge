fee7057670276dc0e17a9631b822775bbf5bb08f
jesusmontano <jesusmontano96@gmail.com>
2019-11-24 13:24:40 -0800

    Remove default test comment

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
-
 import { isTriangle, hasAllPositiveSides, determineType } from './util/helpers';
 
 describe('isTriangle', () => {
